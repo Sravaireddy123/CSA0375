07722ca57d8177d7268d1397112cf29592d4e7bf
Sravaireddy123 <112490847+Sravaireddy123@users.noreply.github.com>
2022-09-19 23:43:57 +0530

    Add files via upload

---
+++ Factorial Without Recursion.DS.cpp
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main()
+ 
+{  
+  int number;  
+  long fact;  
+  printf("Enter a number: ");  
+  scanf("%d", &number);   
+    
+  printf("Factorial of %d is %ld\n", number, fact);  
+  return 0;  
+}
